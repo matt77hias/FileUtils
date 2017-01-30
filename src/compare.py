472ce5520a56e77695e4c71050b43e9270f21b98
ALIEN <matthias.moulin@gmail.com>
2017-01-30 21:17:51 +0100

    update

--- src/compare.py
+++ src/compare.py
@@ -1,46 +1,27 @@
+import cv2
 import numpy as np
 
 from name import concat
 from os import listdir
-from PIL import Image
+
+path = 'C:\Users\Matthias\Documents\Courses\Masterproef\Tests\Session 2.0\A'
 
 #optimization: binary tree comparison
 def check(path):
     for fname1 in listdir(path):
         for fname2 in listdir(path):
             if fname1 < fname2:
-                if not (check_equal_files(concat(path, fname1), concat(path, fname2))):
-                    print('error: ' + fname1 + ' vs ' + fname2)
-                    #save_diff_files(concat(path, fname1 + ' vs ' + fname2 + '.png'), concat(path, fname1), concat(path, fname2))
- 
-from cv2 import imwrite               
-def save_diff_images(fname, A, B):
-    D = diff_images(A, B)
-    imwrite(fname, D)
-    
-def save_diff_files(fname, fname1, fname2):
-    A = PIL2array(Image.open(fname1))
-    B = PIL2array(Image.open(fname2))
-    save_diff_images(fname, A, B)
-    
-def check_equal_images(A, B):
-    return np.max(A - B) == 0
-    
-def check_equal_files(fname1, fname2):
-    return operate(fname1, fname2, check_equal_images)
-
-def diff_images(A, B):
-    return A - B
-    
-def diff_files(fname1, fname2):
-    return operate(fname1, fname2, diff_images)
-    
-def operate(fname1, fname2, f):
-    A = PIL2array(Image.open(fname1))
-    B = PIL2array(Image.open(fname2))
-    return f(A,B)
-    
-def PIL2array(img):
-    return np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
-
-#if __name__ == "__main__":
+                A = cv2.imread(concat(path, fname1), 1)
+                B = cv2.imread(concat(path, fname2), 1)  
+                if not np.array_equal(A, B):
+                    s = fname1 + ' vs ' + fname2
+                    nz = np.nonzero(A - B)
+                    Dout = A.copy()
+                    xs = nz[0] 
+                    ys = nz[1]
+                    for i in range(xs.shape[0]):
+                        Dout[xs[i],ys[i],:] = np.array([0,0,255], dtype=np.uint8)
+                        
+                    print('error [' + str(xs.shape[0]) + ']: ' +  s)
+                    
+                    cv2.imshow(s, Dout)
